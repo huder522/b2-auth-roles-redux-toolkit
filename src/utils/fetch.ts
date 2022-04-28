88d8fc03c6bb83a37866021243dc4f30e457cfde
huder522 <huder522>
2022-04-27 22:58:35 -0500

    feat: fetch with and without token added

---
+++ src/utils/fetch.ts
@@ -0,0 +1,43 @@
+
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
+export const fetchWithoutToken = ( endpoint: string, data: any, method: Method = 'GET' ) => {
+
+  const url: string = `${process.env.REACT_APP_API_URL}${endpoint}`;
+
+  if ( method === 'GET' ) {
+    return fetch( url );
+  } else {
+    return fetch( url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( data ),
+    });
+  }
+}
+
+export const fetchWithToken = ( endpoint: string, data: any, method: Method = 'GET' ) => {
+
+  const url: string = `${process.env.REACT_APP_API_URL}${endpoint}`;
+  const token: string = localStorage.getItem('auth-app-token') || '';
+
+  if ( method === 'GET' ) {
+    return fetch( url, {
+      method,
+      headers: {
+        'x-token': token,
+      }
+    });
+  } else {
+    return fetch( url, {
+      method,
+      headers: {
+        'x-token': token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.parse( data ),
+    })
+  }
+}
