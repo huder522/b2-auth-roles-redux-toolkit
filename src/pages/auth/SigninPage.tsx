644df4789d5ed6427e88c2028814a4f47919d8af
huder522 <huder522>
2022-04-27 22:37:30 -0500

    feat: handling sign in form state

--- src/pages/auth/SigninPage.tsx
+++ src/pages/auth/SigninPage.tsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import { FormEvent } from 'react'
+import { useForm } from '../../hooks/useForm'
 
+
+interface FormData {
+  email: string;
+  password: string;
+}
+const initialForm: FormData = {
+  email: 'peter@user.com',
+  password: '123456'
+}
 export const SigninPage = () => {
+
+  const { formValues, handleInputChange } = useForm( initialForm );
+  const { email, password } = formValues;
+
+  const handleSubmit = ( e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="signin-container">
       <h1>Login</h1>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <input 
           className="form-control mb-3"
           type="text" 
           placeholder="email"
           autoComplete="off"
+          name="email"
+          value={ email }
+          onChange={ handleInputChange }
         />
         <input 
           className="form-control mb-3"
           type="password" 
           placeholder="password"
           autoComplete="off"
+          name="password"
+          value={ password }
+          onChange={ handleInputChange }
         />
         <div className="d-grid gap-2">
           <button
